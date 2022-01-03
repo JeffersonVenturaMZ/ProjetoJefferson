0e97c898727d461ab73206e82ec50dd402a5435c
Jefferson JV. Ventura <jeffersonventura@gac.local>
2022-01-03 12:03:13 -0300

    Correção rodapé

--- Site/application/views/rodape.php
+++ Site/application/views/rodape.php
@@ -1,4 +1,4 @@
 <br class="clear-both"/>
 <div  class="alert-dark">
-	<p style='text-align:center;'>&copy; Copyright 2019 <a href = "#">Jefferson Ventura | INFO-E</a>.</p>
+	<p style='text-align:center;'>&copy; Desenvolvido por <a href="https://www.linkedin.com/in/jefferson-ventura-047682208/">Jefferson Ventura.</p>
 </div>
